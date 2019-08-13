856586a288d238a2227c96230c803e7e45b50d14
Sam Mesch <smeschishnick@gmail.com>
2019-08-13 10:14:35 -0600

    auth progress

--- app.js
+++ app.js
@@ -2,8 +2,65 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mountRoutes from './routes'
 import cors from 'cors';
+import expressSession from 'express-session';
+import passport from 'passport';
+import Auth0Strategy from 'passport-auth0';
+require("dotenv").config();
+const authRouter = require("./auth");
 
 const app = express();
+
+const session = {
+  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  session.cookie.secure = true; // Serve secure cookies, requires HTTPS
+}
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API 
+     * (resource server)
+     * accessToken is the token to call the Auth0 API 
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
 
 const whitelist = ['http://localhost:3000']
 const corsOptions = {
@@ -22,6 +79,7 @@ const corsOptions = {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
+app.use("/", authRouter);
 
 // catch 400
 app.use((err, req, res, next) => {
